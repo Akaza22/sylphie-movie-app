76c9ecb86a7f0de306ebc9bc5ed0024ad26c5a65
Akaza22 <rmrbintang22@gmail.com>
2023-12-23 00:15:39 +0700

    private&public client

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+const baseURL = "https://sylphie-movie-api.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
